dbd8385dcaf276f7373633f39c38a5ed7f6aa423
savvysiddharth <savvysiddharth@gmail.com>
2019-03-30 15:46:20 +0530

    minor changes

--- visBlog/scripts/astar.js
+++ visBlog/scripts/astar.js
@@ -1,35 +1,21 @@
-var cols = 10;
-var rows = 10;
-var grid = new Array(cols);
-
-var openSet = [];
-var closedSet = [];
-var start,end;
-var w,h;
-var path = [];
-
-var stop = false; // stop drawing
-
+let columns = 10, rows = 10;
+let openList = [], closedList = [];
+let start,end;
+let w,h;
+let path = [];
+let stop = false; // stop drawing (used for play pause)
 let hrus = 0; //heuristic
-
 let finished = false;
-var ctr = 0;
+let ctr = 0;
+let grid = new Array(columns);
 
-function removeFromArray(arr,elt) {
-  for (var i = arr.length - 1; i >= 0; i--) {
-    if(arr[i] == elt) {
-      arr.splice(i,1); // remove(where,how many)
-    }
-  }
-}
 
+//this needs to be refactored for using minkowski distance
 function heuristic(a,b) { //educated guess
   if(hrus == 0) {
-    var d = mysketch.dist(a.i, a.j, b.i, b.j); //euclidian distance
-    return d;
+    return mysketch.dist(a.i, a.j, b.i, b.j); //euclidian distance
   } else if(hrus == 1) {
-    var d = abs(a.i - b.i) + abs(a.j - b.j); //manhatten distance
-    return d;
+    return abs(a.i - b.i) + abs(a.j - b.j); //manhatten distance
   }
 }
 
@@ -60,8 +46,7 @@ class Spot {
     mysketch.strokeWeight(1);
     mysketch.fill(col);
     mysketch.ellipse(this.i * w+w/2, this.j * h+w/2, w/2, h/2, w/2, h/2);
-    if(this.wall)
-    {
+    if(this.wall) {
       mysketch.fill(0);
       mysketch.stroke(50);
       mysketch.rect(this.i * w, this.j * h, w, h);
@@ -69,27 +54,27 @@ class Spot {
   }
 
   addNeighbours(grid) {
-    var col = this.i;
-    var row = this.j;
+    let column = this.i;
+    let row = this.j;
 
-    if(col < cols-1)
-      this.neighbours.push(grid[col+1][row]); //right
-    if(col > 0)
-      this.neighbours.push(grid[col-1][row]); //left
+    if(column < columns-1)
+      this.neighbours.push(grid[column+1][row]); //right
+    if(column > 0)
+      this.neighbours.push(grid[column-1][row]); //left
     if(row < rows-1)
-      this.neighbours.push(grid[col][row+1]); //down
+      this.neighbours.push(grid[column][row+1]); //down
     if(row > 0)
-      this.neighbours.push(grid[col][row-1]); //up
+      this.neighbours.push(grid[column][row-1]); //up
 
     //corners
-    if(col < cols-1 && row < rows-1)
-      this.neighbours.push(grid[col+1][row+1]);
-    if(col > 0 && row > 0)
-      this.neighbours.push(grid[col-1][row-1]);
-    if(col > 0 && row < rows-1)
-      this.neighbours.push(grid[col-1][row+1]);
-    if(col < cols-1 && row > 0)
-      this.neighbours.push(grid[col+1][row-1]);
+    if(column < columns-1 && row < rows-1)
+      this.neighbours.push(grid[column+1][row+1]);
+    if(column > 0 && row > 0)
+      this.neighbours.push(grid[column-1][row-1]);
+    if(column > 0 && row < rows-1)
+      this.neighbours.push(grid[column-1][row+1]);
+    if(column < columns-1 && row > 0)
+      this.neighbours.push(grid[column+1][row-1]);
   }
 }
 
@@ -100,33 +85,33 @@ let sketch = (p) => {
 
     const {width,height} = p;
 
-    w = width /cols;
-    h = height/rows;
+    w = width / columns;
+    h = height / rows;
 
     //making 2D array
-    for(var i=0;i<cols;i++) {
+    for(var i = 0 ; i < columns ; i++) {
       grid[i] = new Array(rows);
     }
 
-    for(var i=0;i<cols;i++) {
-      for(var j=0;j<rows;j++) {
+    for(var i = 0 ; i < columns ; i++) {
+      for(var j = 0 ; j < rows ; j++) {
         grid[i][j] = new Spot(i,j);
       }
     }
 
 
-    for(var i=0;i<cols;i++) {
-      for(var j=0;j<rows;j++) {
+    for(var i = 0 ; i < columns ; i++) {
+      for(var j = 0 ; j < rows ; j++) {
         grid[i][j].addNeighbours(grid);
       }
     }
 
     start = grid[0][0];
-    end = grid[cols-1][rows-1];
-    start.wall=false;
+    end = grid[columns-1][rows-1];
+    start.wall = false;
     end.wall = false;
 
-    openSet.push(start);
+    openList.push(start);
     start.h = heuristic(start,end);
     p.background('rgba(255,255,255,0.1)');
   };
@@ -135,23 +120,24 @@ let sketch = (p) => {
     if(!stop) {
       p.frameRate(1);
       ctr++;
-      if(openSet.length > 0) {
+      let current;
+      if(openList.length > 0) {
         //keep going
-        var winner = 0;
-        for(var i=0; i < openSet.length; i++) {
-          if(openSet[i].f < openSet[winner].f) {
-            winner = i;
+        let bestNode = 0; //for the best node from openset
+        for(let i=0; i < openList.length; i++) {
+          if(openList[i].f < openList[bestNode].f) {
+            bestNode = i;
           }
         }
 
-        var current = openSet[winner];
+        current = openList[bestNode];
 
         if(current === end) {
           stop = true; //stopping the draw loop to iterate nothing
 
           //resetting things to initial values
-          for(var i=0;i<cols;i++) {
-            for(var j=0;j<rows;j++) {
+          for(let i=0;i<columns;i++) {
+            for(let j=0;j<rows;j++) {
               grid[i][j].reset();
             }
           }
@@ -160,18 +146,18 @@ let sketch = (p) => {
           finished = true;
         }
 
-        removeFromArray(openSet,current);
-        closedSet.push(current);
+        removeFromArray(openList,current);
+        closedList.push(current);
 
         //calc g
-        var neighbours = current.neighbours;
-        for(var i=0; i<neighbours.length;i++) {
-          var neighbour = neighbours[i];
-          if(!closedSet.includes(neighbour) && !neighbour.wall) {
-            var temp_g = current.g + 1;
+        let neighbours = current.neighbours;
+        for(let i = 0 ; i < neighbours.length ; i++) {
+          let neighbour = neighbours[i];
+          if(!closedList.includes(neighbour) && !neighbour.wall) {
+            let temp_g = current.g + 1;
 
-            var newPath=false;
-            if(openSet.includes(neighbour)) {
+            let newPath=false;
+            if(openList.includes(neighbour)) {
               if(temp_g < neighbour.g) {
                 neighbour.g = temp_g;
                 newPath=true;
@@ -179,7 +165,7 @@ let sketch = (p) => {
             }
             else {
               neighbour.g = temp_g;
-              openSet.push(neighbour);
+              openList.push(neighbour);
               newPath=true;
             }
 
@@ -193,9 +179,7 @@ let sketch = (p) => {
         }
       }
       else {
-        //no soln
-        console.log('NO SOLUTION')
-        // noLoop();
+        console.log('CANNOT FIND!')
         stop = true;
         return;
       }
@@ -204,8 +188,8 @@ let sketch = (p) => {
       p.background(50);
 
       p.strokeWeight(1);
-      for(var i=0;i<cols;i++) {
-        for(var j=0;j<rows;j++) {
+      for(let i = 0 ; i < columns ; i++) {
+        for(let j = 0 ; j < rows ; j++) {
           grid[i][j].show(p.color(255));
         }
       }
@@ -214,26 +198,25 @@ let sketch = (p) => {
       p.fill('rgba(0,240,0,0.5)');
       p.rect(end.i * w, end.j * h, w, h);
 
-      for(var i=0; i<closedSet.length ; i++) {
-        closedSet[i].show(p.color(226, 45, 45)); //red
+      for(let i = 0; i < closedList.length ; i++) {
+        closedList[i].show(p.color(226, 45, 45)); //red
       }
 
-      for(var i=0; i<openSet.length ; i++) {
-        openSet[i].show(p.color(58, 206, 41)); //green
+      for(let i = 0; i < openList.length ; i++) {
+        openList[i].show(p.color(58, 206, 41)); //green
       }
 
       path = [];
-      var temp = current;
+      let temp = current;
       path.push(temp);
 
-      while(temp.previous)
-      {
+      while(temp.previous) {
         path.push(temp.previous);
         temp = temp.previous;
       }
 
 
-      for(var i=0;i<path.length-1;i++) {
+      for(let i = 0 ; i < path.length - 1 ; i++) {
         p.strokeWeight(6);
         p.stroke(108, 161, 247); //line color
         p.line(path[i].i*w +w/2, path[i].j*h +h/2,path[i+1].i*w +w/2, path[i+1].j*h+h/2);
@@ -241,6 +224,14 @@ let sketch = (p) => {
     }
   };
 };
+
+function removeFromArray(arr,elt) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if(arr[i] == elt) {
+      arr.splice(i,1); // remove(where,how many)
+    }
+  }
+}
 
 let mysketch = new p5(sketch,"sketchbox");
 
@@ -272,38 +263,42 @@ function resetSketch() {
     btn.innerHTML = "Pause";
   }
 
-  openSet = [];
-  closedSet = [];
+  openList = [];
+  closedList = [];
   path = [];
 
-  w = width /cols;
-  h = height/rows;
+  w = width / columns;
+  h = height / rows;
 
   //making 2D array
-  for(var i=0;i<cols;i++) {
+  for(let i = 0 ; i < columns ; i++) {
     grid[i] = new Array(rows);
   }
 
-  for(var i=0;i<cols;i++) {
-    for(var j=0;j<rows;j++) {
+  for(let i = 0 ; i < columns ; i++) {
+    for(let j = 0 ; j < rows ; j++) {
       grid[i][j] = new Spot(i,j);
     }
   }
 
 
-  for(var i=0;i<cols;i++) {
-    for(var j=0;j<rows;j++) {
+  for(let i = 0 ; i < columns ; i++) {
+    for(let j = 0 ; j < rows ; j++) {
       grid[i][j].addNeighbours(grid);
     }
   }
 
   start = grid[0][0];
-  end = grid[cols-1][rows-1];
+  end = grid[columns-1][rows-1];
   start.wall=false;
   end.wall = false;
 
-  openSet.push(start);
+  openList.push(start);
   start.h = heuristic(start,end);
 
   finished = false;
 }
+
+/**
+* Design inspired from - https://github.com/CodingTrain/AStar
+*/
